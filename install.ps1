984fbcadcef6c489f85fb1cd196d3a83ce0c8e33
ZodiacTeamOS <zodiacteam0s@gmail.com>
2025-12-23 04:25:59 +0200

    Delete install.ps1

--- install.ps1
+++
@@ -1,5 +0,0 @@
-$u = "https://raw.githubusercontent.com/ZodiacTeamOS/Add-MicrosoftStore-LTSC/main/script.ps1"
-$f = "$env:TEMP\store.ps1"
-
-irm $u -OutFile $f
-powershell -ExecutionPolicy Bypass -File $f
