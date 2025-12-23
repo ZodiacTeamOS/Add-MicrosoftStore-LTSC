e15e32111de1c941f8fbb2ba54268697c4a7ded4
ZodiacTeamOS <zodiacteam0s@gmail.com>
2025-12-23 04:07:20 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,1 +1,5 @@
+$u = "https://raw.githubusercontent.com/ZodiacTeamOS/Add-MicrosoftStore-LTSC/the_bun/script.ps1"
+$f = "$env:TEMP\store.ps1"
 
+irm $u -OutFile $f
+powershell -ExecutionPolicy Bypass -File $f
